5f27a4795228d5824fd221203dd94c6cbab5c3b0
Aa4412382010 <51355593+Aa4412382010@users.noreply.github.com>
2019-09-19 21:25:19 +0800

    Add files via upload

--- 0609-zhoutao/music/src/components/MusicPlayer.vue
+++ 0609-zhoutao/music/src/components/MusicPlayer.vue
@@ -1,6 +1,6 @@
 <template>
     <el-col class="palyer" :span="18" hidden>
-    <aplayer theme="#409eff" showLrc :music="album[0]" :list="album" listFolded/>
+    <aplayer theme="#409eff" showLrc :music="album[0]" :list="album" listFolded repeat="list"/>
     </el-col>
 </template>
 
@@ -14,28 +14,23 @@
         props:['music'],
         data(){
             return{
-                album:[{
-                    title: '',
-                    artist: '',
-                    src: '',
-                    pic: '',
-                    lrc: ''
+            album:[{
+                    title: '光年之外',
+                    artist: 'G.E.M. 邓紫棋',
+                    src: 'http://localhost:8080/music/G.E.M. 邓紫棋 - 光年之外.mp3',
+                    pic: require("../../public/pic/光年之外.png"),
+                    lrc: 'http://localhost:8080/lrc/光年之外.lrc'
                 }],
             }
         },
         methods:{
             play(music){
-                if(!this.album[0].title){
+                document.getElementsByClassName('palyer')[0].hidden =false
+                if(this.album.indexOf(music)>-1){
+                    let add_music = this.album.splice(this.album.indexOf(music),1)[0]
+                    this.album.unshift(add_music)
+                }else{ 
                     this.album.unshift(music)
-                    this.album.pop()
-                    document.getElementsByClassName('palyer')[0].hidden =false
-                }else{
-                    if(this.album.indexOf(music)>-1){
-                        let add_music = this.album.splice(this.album.indexOf(music),1)[0]
-                        this.album.unshift(add_music)
-                    }else{
-                        this.album.unshift(music)
-                    }
                 }
             }
         }
